d3e8947f9047fe16fe690ffb83e473bcd59aa6dd
sanishjohn <sanishj@gmail.com>
2017-06-22 21:21:28 -0500

    moved the JS scripts from index to app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,63 @@
 $(document).ready(function() {
-    
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAfPrXNiyLnAF9X7NscB0kXXAwyz4evTB4",
+        authDomain: "newsbusters-14959.firebaseapp.com",
+        databaseURL: "https://newsbusters-14959.firebaseio.com",
+        projectId: "newsbusters-14959",
+        storageBucket: "newsbusters-14959.appspot.com",
+        messagingSenderId: "801909748364"
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    var analizedWord = "";
+    var positivityScore = "";
+
+    var word = {
+        value: "Clinton",
+        score: 50
+    }
+
+    // database.ref().push(word);
+
+    // Attaching APIs
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=cat&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // Performing our AJAX GET request
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After the data comes back from the API
+        .done(function(response) {
+            console.log(response);
+        });
+
+    //split string into array of words dropping punctuation
+    var sentence = "The quick; brown, fox jumped. over the lazy dog"
+    var arrayOfWords = sentence.split(/\W+/);
+
+    console.log('The original string is: "' + sentence + '"');
+    console.log(arrayOfWords);
+
+
+    // Built by LucyBot. www.lucybot.com
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
+        'begin_date': "20170101",
+        'end_date': "20170102"
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+    }).fail(function(err) {
+        throw err;
+    });
 });
